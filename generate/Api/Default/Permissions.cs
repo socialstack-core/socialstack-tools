14732b64fcff568c2263e0fe052a004f3b2f8e77
Luke Briggs <luke@kulestar.com>
2020-05-13 16:05:32 +0100

    Added permissions sample

---
+++ generate/Api/Default/Permissions.cs
@@ -0,0 +1,45 @@
+﻿using Api.Startup;
+using Api.Eventing;
+using Api.Contexts;
+using Api.Permissions;
+using System.Threading.Tasks;
+
+namespace Api.Entities
+{
+	/// <summary>
+	/// Instances capabilities during the very earliest phases of startup.
+	/// </summary>
+	[EventListener]
+	public class Permissions
+	{
+		/// <summary>
+		/// Instanced automatically.
+		/// </summary>
+		public Permissions()
+		{
+			// Hook the default role setup. It's done like this so it can be removed by a plugin if wanted.
+			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
+			{
+				/*
+				Example permission rules.
+				
+				Member role: A verified user account. Not an admin.
+				Guest role: A user account. The transition from guest to member is up to you.
+				Public role: Not logged in at all.
+				
+				// Allow public creation (as it's disabled by default):
+				Roles.Member.Grant("entity_create");
+				Roles.Public.Grant("entity_create");
+				Roles.Guest.Grant("entity_create");
+				
+				// Remove public viewing (as it's enabled by default):
+				Roles.Guest.Revoke("entity_load", "entity_list");
+				Roles.Public.Revoke("entity_load", "entity_list");
+				Roles.Member.Revoke("entity_load", "entity_list");
+				*/
+				
+				return Task.FromResult(source);
+			}, 20);
+		}
+	}
+}
