2ea7bacd050ce5424b64a3307ff8aad8e798f0b3
Luke Briggs <luke@kulestar.com>
2020-05-26 12:17:44 +0100

    Added propTypes info

--- generate/UI/Default/Entity.js
+++ generate/UI/Default/Entity.js
@@ -11,3 +11,22 @@ export default class Entity extends React.Component {
 	}
 	
 }
+
+/*
+// propTypes are used to describe configuration on your component in the editor.
+// Just setting it to an empty object will make your component appear as something that can be added.
+// Define your available props like the examples below.
+
+Entity.propTypes = {
+	
+	title: 'string', // text input
+	size: [1,2,3,4], // dropdowns
+	
+	// All <Input type='x' /> values are supported - checkbox, color etc.
+	// Also the special id type which can be used to select some other piece of content (by entity name), like this:
+	templateToUse: {type: 'id', content: 'Template'}
+	
+};
+
+Entity.icon='align-center'; // fontawesome icon
+*/
