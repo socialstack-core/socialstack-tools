d468991c502218a006449db22dff1c890d57dd3c
Luke Briggs <luke@kulestar.com>
2020-09-26 03:51:00 +0100

    Changed default permission response to handle the new non-allocating event flow

--- generate/Api/Default/Permissions.cs
+++ generate/Api/Default/Permissions.cs
@@ -38,7 +38,7 @@ namespace Api.Entities
 				Roles.Member.Revoke("entity_load", "entity_list");
 				*/
 				
-				return Task.FromResult(source);
+				return new ValueTask<object>(source);
 			}, 20);
 		}
 	}
