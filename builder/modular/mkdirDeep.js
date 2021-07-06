25796a4325107a71e5ea4d505692847195800b79
Luke Briggs <luke@kulestar.com>
2021-07-06 13:22:15 +0100

    mkdir verbose

--- builder/modular/mkdirDeep.js
+++ builder/modular/mkdirDeep.js
@@ -5,6 +5,7 @@ module.exports = function(pathname) {
 	let paths = pathname.split(path.sep);
 	for (let i = 1; i <= paths.length; i++) {
 		let dirpath = path.join.apply(path, paths.slice(0, i));
+		console.log(dirpath);
 		!fs.existsSync(dirpath) && fs.mkdirSync(dirpath);
 	}
 };
