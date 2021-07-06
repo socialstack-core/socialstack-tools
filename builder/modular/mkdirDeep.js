0346ddfbbf4aef31cb5c636ca68e8752bc5351d9
Luke Briggs <luke@kulestar.com>
2021-07-06 13:45:24 +0100

    Added some more mkdir resilience

--- builder/modular/mkdirDeep.js
+++ builder/modular/mkdirDeep.js
@@ -2,9 +2,10 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(pathname) {
+	pathname = path.resolve(pathname);
 	let paths = pathname.split(path.sep);
-	for (let i = 2; i <= paths.length; i++) {
-		let dirpath = paths.slice(0, i).join(path.sep);
+	for (let i = 1; i < paths.length; i++) {
+		let dirpath = paths.slice(0, i+1).join(path.sep);
 		!fs.existsSync(dirpath) && fs.mkdirSync(dirpath);
 	}
 };
