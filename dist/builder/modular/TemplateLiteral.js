d8da61b853e538b924f2b132e48388a731923deb
Luke Briggs <luke@kulestar.com>
2026-03-01 22:42:50 +0000

    Removed dist

--- dist/builder/modular/TemplateLiteral.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-nocheck
-/// <summary>
-/// Information about a template literal.
-/// </summary>
-class TemplateLiteral {
-}
-exports.default = TemplateLiteral;
